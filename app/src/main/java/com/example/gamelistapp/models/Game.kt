5f1189f730574738edd5093be3600d5b14fa0dc7
Rodrigo do Vale Stankowicz <rodrigo.vale.st@gmail.com>
2025-05-19 22:19:53 -0300

    deleting project

--- app/src/main/java/com/example/gamelistapp/models/Game.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gamelistapp.models
-
-data class Game(
-    val name: String,
-    val company: String,
-    val releaseDate: String,
-    val genre: String,
-    val description: String
-)
